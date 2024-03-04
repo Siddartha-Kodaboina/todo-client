9bec92edbcc2d7862f8b60bb6207f8b20e96ad95
Siddartha Kodaboina <k.siddartha49@gmail.com>
2024-03-04 05:11:19 -0800

    Added changes to fix the issues in production

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import useFirebaseUser from './hooks/useFirebaseUser';
 import NavigationBar from './components/NavigationBar';
 
 function App() {
-  const user = useFirebaseUser();
   console.log('Process.env :', process.env);
+  const user = useFirebaseUser();
+  // console.log('Process.env :', process.env);
 
   const base_url = process.env.REACT_APP_NODE_ENV === 'development' ? process.env.REACT_APP_LOCAL_BASE_URL: process.env.REACT_APP_SERVER_BASE_URL;
   console.log('Base URL :', base_url);
